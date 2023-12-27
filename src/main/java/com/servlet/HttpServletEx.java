78021ef6dca7e68b179942046266f1eb66036d85
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-27 22:51:13 +0530

    different Servlet types

---
+++ src/main/java/com/servlet/HttpServletEx.java
@@ -0,0 +1,7 @@
+package com.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class HttpServletEx extends HttpServlet {
+
+}
---
+++ src/main/java/com/servlet/OldServletEx.java
@@ -0,0 +1,38 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class OldServletEx implements Servlet{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		
+	}
+
+}
