41b602298df4570f0a7b93f9e55d961ff11b44c6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-28 09:44:13 +0530

    <url-pattern>/a</url-pattern>

--- src/main/java/com/serv/DemoServlet.java
+++ src/main/java/com/serv/DemoServlet.java
@@ -1,7 +1,25 @@
 package com.serv;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8775019552842459616L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		super.doGet(req, resp);
+		PrintWriter pw = resp.getWriter();
+					pw.append("Inside doGet()");
+		System.out.println("Inside the doGet(),");
+	}
 }
