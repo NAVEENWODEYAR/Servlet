928757ed292f69cf178565c3676c5cd2f782cf0a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-30 12:33:36 +0530

    HTTP method GET is not supported by this URL

--- src/main/java/com/serv/DeleteAccount.java
+++ src/main/java/com/serv/DeleteAccount.java
@@ -9,20 +9,25 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-//@WebServlet("/register")
-public class RegisterServlet extends HttpServlet {
-
+@WebServlet("/delete")
+public class DeleteAccount extends HttpServlet {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 4920995325265453807L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		super.doGet(req, resp);
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		super.doPost(req, resp);
 		PrintWriter pw = resp.getWriter();
-					pw.write("Please register here,");
+					pw.write("Delete your account here,");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+		PrintWriter pw = resp.getWriter();
+					pw.write("Delete your account here,");
 		System.out.println("Inside doGet(),");
 	}
 }
