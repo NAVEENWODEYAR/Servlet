370096b5bb297b5e72732fe4b23dc6ec65f3f778
Naveen <naveenkrishnawodeyar@gmail.com>
2023-12-31 01:12:14 +0530

    index.jsp added

--- src/main/java/com/serv/RegisterServlet.java
+++ src/main/java/com/serv/RegisterServlet.java
@@ -19,7 +19,7 @@ public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //		super.doGet(req, resp);
 		PrintWriter pw = resp.getWriter();
 					pw.write("Please register here,");
